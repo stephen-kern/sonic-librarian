3827c47e907ec6c67622bee3a16aeecaaf54aa06
Stephen Kern <stephenkern96@gmail.com>
2025-12-25 11:53:03 -0600

    project setup. test db connection. test fastapi connection.

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in environment variables.")
+
