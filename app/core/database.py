cf16942de9e80a49077d2da2bc60d1af6e5464bc
Stephen Kern <stephenkern96@gmail.com>
2026-01-03 18:11:16 -0600

    FastAPI and DB connection successful

--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,10 @@
-# The connection betwenn our Python app and the database
-
+# The connection between our Python app and the database
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker
 from app.core.config import DATABASE_URL
 from app.models.base import Base
+from typing import Generator
+from sqlalchemy.orm import Session
 
 
 # Engine and SessionLocal are the core components for database interaction
@@ -18,3 +19,10 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try: 
+        yield db # hands the session to fastAPI
+    finally:
+        db.close() # cleans up the session after use
--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from app.core.database import get_db
 
-app = FastAPI()
+app = FastAPI(
+    title="Sonic Librarian",
+    description="A fast and efficient music library manager.",
+    version="0.1.0"
+)
 
 @app.get("/health")
-def health_check():
+def health_check(db: Session = Depends(get_db)):
+    # If DB connection fails, this endpoing fails
     return {"status": "ok"}
